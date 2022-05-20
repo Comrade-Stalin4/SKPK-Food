ee5c608f62168c8e732faf871034abcb7b3cb66a
Joseph Stalin <105896451+Comrade-Stalin4@users.noreply.github.com>
2022-05-20 12:18:45 +0700

    First upload

---
+++ project ketujuh -SKPK food/database.js
@@ -0,0 +1,103 @@
+let menu = []
+
+let makanan = {
+    nama: "Jus",
+    kode: "BRG001",
+    stok: 10,
+    harga: 5000,
+    rating: 5.0,
+    gambar: "https://media.suara.com/pictures/653x366/2016/05/23/o_1ajds7pv9uhhlsn1mb41jvu9iha.jpg"
+}
+    menu.push(makanan);
+
+    let makanan_2 = {
+        nama: "Bakso",
+        kode: "BRG002",
+        stok: 20,
+        harga: 15000,
+        rating: 5.0,
+        gambar: "https://cdn-2.tstatic.net/tribunnews/foto/bank/images/bakso-sapi-kuah-1.jpg"
+    }
+        menu.push(makanan_2);
+
+        let makanan_3 = {
+            nama: "Heci",
+            kode: "BRG003",
+            stok: 100,
+            harga: 1000,
+            rating: 5.0,
+            gambar: "https://i.ytimg.com/vi/swQq5Kt1Svw/maxresdefault.jpg"
+        }
+            menu.push(makanan_3);
+
+            let makanan_4 = {
+                nama: "Permen",
+                kode: "BRG004",
+                stok: 1000,
+                harga: 500,
+                rating: 5.0,
+                gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZjT91A0_co8m_Y8cojNYMwxs8POole1QtGg&usqp=CAU"
+            }
+                menu.push(makanan_4);
+
+                let makanan_5 = {
+                    nama: "Air Mineral",
+                    kode: "BRG005",
+                    stok: 50,
+                    harga: 3500,
+                    rating: 5.0,
+                    gambar: "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2021/6/16/569ca437-8ede-4afe-a12c-e6c7c6f20fd4.jpg"
+                }
+                    menu.push(makanan_5);
+
+                    let makanan_6 = {
+                        nama: "Shashlik",
+                        kode: "BRG006",
+                        stok: 60,
+                        harga: 50000,
+                        rating: 5.0,
+                        gambar: "https://awsimages.detik.net.id/community/media/visual/2016/12/20/d90ee7e3-b183-4592-9a29-390e7f0e6647.jpg?w=700&q=90"
+                    }
+                        menu.push(makanan_6);
+
+                        let makanan_7 = {
+                            nama: "Croffle",
+                            kode: "BRG007",
+                            stok: 50,
+                            harga: 20000,
+                            rating: 5.0,
+                            gambar: "https://asset.kompas.com/crops/LMoYWuQ5In78LGHYkfCM3Gt5w4M=/127x97:873x594/750x500/data/photo/2021/07/09/60e7c85cd748b.jpg"
+                        }
+                            menu.push(makanan_7);
+
+                            let makanan_8 = {
+                                nama: "Sate",
+                                kode: "BRG008",
+                                stok: 45,
+                                harga: 10000,
+                                rating: 5.0,
+                                gambar: "http://kbu-cdn.com/dk/wp-content/uploads/sate-ayam.jpg"
+                            }
+                                menu.push(makanan_8);
+
+                                let makanan_9 = {
+                                    nama: "Kompot",
+                                    kode: "BRG009",
+                                    stok: 50,
+                                    harga: 25000,
+                                    rating: 5.0,
+                                    gambar: "https://littlesunnykitchen.com/wp-content/uploads/2019/08/Kompot-homemade-juice-6.jpg"
+                                }
+                                    menu.push(makanan_9);
+
+                                    let makanan_10 = {
+                                        nama: "Sauerkraut",
+                                        kode: "BRG0010",
+                                        stok: 25,
+                                        harga: 12000,
+                                        rating: 5.0,
+                                        gambar: "https://cms.sehatq.com/cdn-cgi/image/f=auto,width=480,fit=pad,background=white,quality=100/public/img/article_img/mengenal-sauerkraut-kubis-asam-khas-jerman-yang-kaya-manfaat-1598499417.jpg"
+                                    }
+                                        menu.push(makanan_10);
+
+                                        console.log(menu);
